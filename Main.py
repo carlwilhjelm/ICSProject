c66a65d97772d7e86524d4b11a9e9f381884266f
carlwilhjelm <carlwilhjelm@gmail.com>
2018-11-27 11:48:18 -0500

    Centroid selection update
    Fixed nan value issue for centroids, added color values to plot to show outliers

--- Main.py
+++ Main.py
@@ -1,15 +1,10 @@
-import re
 import numpy as np
 import matplotlib.pyplot as plt
 from nltk import word_tokenize as tok
-from numpy import *
 from scipy import linalg as SPLA
 import utils
 
-# localDir = "D:\School\GSU\Fall 2018\Intro to Cybersecurity\project\\"
-testFile = "syslogTest.txt"
-
-reDateTime = re.compile(r"^.*?(?= alina)")
+testFile = r"C:\Users\Carl Wilhjelm\PycharmProjects\ICSProject\logs\syslogTest.txt"
 
 # create tokenized list of all logs
 syslog = []
@@ -18,12 +13,10 @@ with open(testFile, 'r') as f:
 
 for line in syslogText:
     syslog.append(tok(line))
-    # print(line)
 n = len(syslog)
 print(n)
-# print(syslog)
-# n = 100
-opNumber = int(n * (0.05))
+opNumber = int(n * (0.01))
+
 # create naive distance matrix for all logs
 distanceMatrix = [[0 for x in range(n)] for y in range(n)]
 for i in range(n):
@@ -37,47 +30,53 @@ for i in range(n):
             k = m
 
         for e in range(k):
-            if syslog[i][e] is not syslog[j][e]:
+            if syslog[i][e] != syslog[j][e]:
                 distance += 1
         distanceMatrix[i][j] = distance / k
         distanceMatrix[j][i] = distanceMatrix[i][j]
 
-avg = np.sum(distanceMatrix, axis=0)
-for i in range(n):
-    avg[i] /= n
-for i in range(n):
-    for j in range(n):
-        distanceMatrix[i][j] -= avg[j]
 cov = np.cov(distanceMatrix, rowvar=False)
+
 k = 2
 w, v = SPLA.eigh(cov, eigvals=(n - k, n - 1))
 U = np.array([v.T[1], v.T[0]])
-distanceMatrix = array(distanceMatrix)
+distanceMatrix = np.array(distanceMatrix)
 TransformedData = np.dot(U, distanceMatrix.T)
-# plot each point (x(n), y(n))
-# print(TransformedData)
-# print(TransformedData.shape)
-# print(TransformedData.T[0, :])
-# plt.plot(TransformedData[0, :], TransformedData[1, :], 'ro')
-# plt.show()
+
+
 dataSet = TransformedData.T
-print(dataSet)
-centroids, clusterAssment = utils.kMeans(dataSet, 3,syslog)# 3 is the k for k-means. dataset is the nx2 matrix after PCA
+# 3 is the k for k-means. dataset is the nx2 matrix after PCA
+centroids, clusterAssment = utils.kMeans(dataSet, 3, syslog)
 print('cluster center are:')
 print(centroids)
-a = array(clusterAssment) # clusterAssment is a list like [[1,12],[2,13],[3,10]], in which 1,2,3 are cluster and 12,13,10 is the distance from cluster center to the data point
-sortedList = list(argsort(a[:, 1]),)# sortedList is a list contain index. like [13,14, 1 ,2.....] 13 means the first element in a is the 13rd largest one.
+# clusterAssment is a list like [[1,12],[2,13],[3,10]],
+# in which 1,2,3 are cluster and 12,13,10 is the distance from cluster center to the data point
+a = np.array(clusterAssment)
+# sortedList is a list contain index. like [13,14, 1 ,2.....]
+# 13 means the first element in a is the 13rd largest one.
+sortedList = list(np.argsort(a[:, 1]),)
 topList = []
 
-for i in range(opNumber):# opNumber is the 5% of data points
-    index = sortedList.index(n - 1 - i)# index(n-1-i), I want to get the 999th,998th,997th's index in the sortedList
-    topList.append(dataSet[index])# then I use the index to get the  999th,998th,997th largest distance in the dataset.
+# opNumber is the 5% of data points
+for i in range(opNumber):
+    # index(n-1-i), I want to get the 999th,998th,997th's index in the sortedList
+    index = sortedList.index(n - 1 - i)
+    # then I use the index to get the  999th,998th,997th largest distance in the dataset.
+    topList.append(dataSet[index])
     print("top record is:")
-    print(dataSet[index],syslog[i])# syslog[i] is the ith record in original data
-# print(sortedList)
-# index = np.where(sortedList == 97)
-# index.getfield(array)
-# print(index)
-# for i in range(opNumber)；
-#     sortedList.__index__()
-# for i in range(shape(dataSet)[0]):
+    # syslog[i] is the ith record in original data
+    print(dataSet[index], syslog[i])
+
+outliers = np.array(topList)
+print("\noutliers")
+print(outliers)
+
+# plot each point (x(n), y(n))
+print()
+print(TransformedData)
+print(TransformedData.shape)
+print(TransformedData.T[0, :])
+plt.plot(TransformedData[0, :], TransformedData[1, :], 'go')
+plt.plot(outliers[:, 0], outliers[:, 1], 'ro')
+plt.plot(centroids[:, 0], centroids[:, 1], 'bo')
+plt.show()
--- utils.py
+++ utils.py
@@ -1,45 +1,53 @@
-from numpy import *
+import numpy as np
 
 
 # calculate Ecli distance
 def distEclud(vecA, vecB):
-    return sqrt(sum(power(vecA - vecB, 2)))
+    return np.sqrt(sum(np.power(vecA - vecB, 2)))
 
 
 # randomly choose k center
-def randCenter(dataSet, k):
-    n = shape(dataSet)[1]
-    centroids = mat(zeros((k, n)))
-    for j in range(n):
-        minJ = min(dataSet[:, j])
-        rangeJ = float(max(dataSet[:, j]) - minJ)
-        centroids[:, j] = minJ + rangeJ * random.rand(k, 1)
+def randCenter(dataSetIn, kIn):
+    while True:
+        centroids = dataSetIn[np.random.choice(range(len(dataSetIn)), kIn, replace=False)]
+        uniques = np.unique(centroids)
+        if len(centroids) == len(uniques)/2:
+            break
+    print("initialized centroids to ")
+    print(centroids)
     return centroids
 
-
 # do cluster
-def kMeans(dataSet, k, syslog, distMeas=distEclud, createCenter=randCenter):
-    m = shape(dataSet)[0]  # row of dataset,dataset is [x,y]
-    clusterAssment = mat(zeros((m, 3)))  # create matrix [cluster,distance, index of syslog] like [0,19, 2]
-    centriods = createCenter(dataSet, k)
+def kMeans(dataSetIn, kIn, syslog):
+    # row of dataset,dataset is [x,y]
+    # m = np.shape(dataSet)[0]
+    m = len(syslog)
+    # create matrix [cluster,distance, index of syslog] like [0,19, 2]
+    clusterAssment = np.mat(np.zeros((m, 3)))
+    centriods = randCenter(dataSetIn, kIn)
     clusterChange = True
     while clusterChange:
         clusterChange = False
         for i in range(m):
-            minDist = inf;
+            minDist = np.inf
             minIndex = -1
-            for j in range(k):
-                distJI = distMeas(centriods[j, 0:1], dataSet[i, 0:1])
+            for j in range(kIn):
+                distJI = distEclud(centriods[j, 0:1], dataSetIn[i, 0:1])
                 if distJI < minDist:
-                    minDist = distJI;
+                    minDist = distJI
                     minIndex = j
             if clusterAssment[i, 0] != minIndex:
-                clusterChange = True;
-            clusterAssment[i, :] = minIndex, minDist ** 2, i
-        print(centriods)
-        for cent in range(k):
-            ptsInCluster = dataSet[nonzero(clusterAssment[:, 0].A == cent)[0]]
-            centriods[cent, :] = mean(ptsInCluster, axis=0)
-    # the clusterAssment is a nx3 matrix, each record is like [0,19,2] which means
-    # this record is in cluster 0, distance from cluster center=19, it is the 2rd row record in original syslog dataset
+                clusterChange = True
+            clusterAssment[i, :] = minIndex, minDist, i
+
+        for cent in range(kIn):
+            count = 0
+            meanX = 0
+            meanY = 0
+            for i in range(m):
+                if clusterAssment[i, 0] == cent:
+                    meanX += dataSetIn[i, 0]
+                    meanY += dataSetIn[i, 1]
+                    count += 1
+            centriods[cent] = np.array([meanX/count, meanY/count])
     return centriods, clusterAssment
